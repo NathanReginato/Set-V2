f871e00ab32ded904f19261c67399e5c7b5a1019
Nathan Reginato <nathan.reginato@gmail.com>
2016-07-19 17:02:48 -0600

    All tests pass

--- spec/createElementSpec.js
+++ spec/createElementSpec.js
@@ -5,7 +5,7 @@ describe("Create element suit", function() {
   let element = new Element([0,0,0,0])
 
   it("will return a div element", function() {
-    expect(element.getElement()).toEqual("<div></div>");
+    expect(element.getElement()).toEqual(`<div class="card red oval empty"></div>`);
   });
 
 });
